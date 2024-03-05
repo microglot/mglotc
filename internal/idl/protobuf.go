eab1ee14ec82a2596f5bdde30c7a694282648a65
Kevin Conway <kevin@microglot.org>
2024-03-05 17:28:55 -0600

    WIP: Allow conflicting enumerants from mgdl

--- internal/idl/protobuf.go
+++ internal/idl/protobuf.go
@@ -10,6 +10,7 @@ var PROTOBUF_TYPE_UIDS = map[string]uint64{
 	"FileOptionsGoPackage": 3,
 	"JsonName":             4,
 	"Proto3Optional":       5,
+	"EnumFromProto":        6,
 }
 
 var PROTOBUF_IDL = fmt.Sprintf(`
@@ -31,9 +32,11 @@ annotation NestedTypeInfo(struct, enum) :NestedTypes @%d
 annotation FileOptionsGoPackage(module) :Text @%d
 annotation JsonName(field) :Text @%d
 annotation Proto3Optional(field) :Bool @%d
+annotation EnumFromProto(enum) :Bool @%d
 `, PROTOBUF_TYPE_UIDS["Package"],
 	PROTOBUF_TYPE_UIDS["NestedTypeInfo"],
 	PROTOBUF_TYPE_UIDS["FileOptionsGoPackage"],
 	PROTOBUF_TYPE_UIDS["JsonName"],
 	PROTOBUF_TYPE_UIDS["Proto3Optional"],
+	PROTOBUF_TYPE_UIDS["EnumFromProto"],
 )
